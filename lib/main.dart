095aaee6f5c16b52a114e3d95ca2c61536d9589a
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-01-08 09:31:15 +0545

    disable landscape mode in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:batch_student_starter/helper/objectbox.dart';
 import 'package:batch_student_starter/state/objectbox_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app/app.dart';
 
@@ -8,8 +9,18 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Create an object for ObjectBoxInstanc
   ObjectBoxState.objectBoxInstance = await ObjectBoxInstance.init();
+
+  //Disable landscape mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    ((value) => runApp(
+      const MyApp(),
+    )
+  ));
   
-  runApp(
-    const MyApp(),
-  );
+  // runApp(
+  //   const MyApp(),
+  // );
 }
